1c69ca80b6d5a14fc6b5224fa345861448d17dcd
vijaykarthik <nvijaykarthik@gmail.com>
2018-02-15 12:24:10 +0530

    updated mongo db schema and other parts

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,7 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 var homelist = function (req, res) {
-    res.render("locations-list", { title: "homelist" })
+    res.render("locations-list",
+        {
+            title: 'Loc8r - find a place to work with wifi',
+            pageHeader: {
+                title: 'Loc8r',
+                strapline: 'Find places to work with wifi near you!'
+            },
+            locations: [{
+                name: 'Starcups',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 3,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '100m'
+            }, {
+                name: 'Cafe Hero',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 4,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '200m'
+            }, {
+                name: 'Burger Queen',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 2,
+                facilities: ['Food', 'Premium wifi'],
+                distance: '250m'
+            }]
+        }
+    );
 }
 var locationInfo = function (req, res) {
     res.render("locations-info", { title: "locationInfo" })
